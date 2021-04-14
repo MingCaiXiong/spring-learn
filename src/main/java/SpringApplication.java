91eca57cfd06375d34988edbf4eac66afaf25279
xmc <xmc000@icloud.com>
2021-04-14 10:22:55 +0800

    <bean> name="apple2, apple, apple3 " 可以多标识

--- src/main/java/SpringApplication.java
+++ src/main/java/SpringApplication.java
@@ -4,6 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        Apple apple3 = context.getBean("apple3", Apple.class);
+        System.out.println(apple3.getTitle());
         //output
         //基于静态工厂
         // 通过无参构建方法构建对象
