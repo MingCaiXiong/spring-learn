fce1c7e14515b7ebda03ab25250f57d1b2f1a3f5
xmc <xmc000@icloud.com>
2021-04-14 21:10:31 +0800

    method or destroy

--- src/main/java/SpringApplication.java
+++ src/main/java/SpringApplication.java
@@ -1,7 +1,12 @@
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import top.xiongmingcai.ioc.entity.Order;
 
 public class SpringApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        Order order1 = context.getBean("order1", Order.class);
+        System.out.println("order1.getTotal() = " + order1.getTotal());
+
+        context.registerShutdownHook();
     }
 }
