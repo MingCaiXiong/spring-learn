1b7212fa1be20be701110f200990ac8cc515789b
xmc <xmc000@icloud.com>
2021-04-14 20:47:21 +0800

    初始化项目

--- src/main/java/SpringApplication.java
+++ src/main/java/SpringApplication.java
@@ -1,14 +1,7 @@
-import com.alibaba.fastjson.JSON;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import top.xiongmingcai.ioc.entity.Pen;
-import top.xiongmingcai.ioc.entity.SchoolBag;
 
 public class SpringApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-
-        context.getBean("a1");
-        context.getBean("b1");
-        context.getBean("c1");
     }
 }
--- src/main/java/top/xiongmingcai/ioc/entity/A.java
+++
@@ -1,7 +0,0 @@
-package top.xiongmingcai.ioc.entity;
-
-public class A {
-    public A() {
-        System.out.println("A" );
-    }
-}
