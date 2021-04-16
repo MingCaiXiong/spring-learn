ca564e0906a1407e0707eeec7945703679b241b6
xmc <xmc000@icloud.com>
2021-04-16 17:10:32 +0800

    注意捕获

--- src/main/java/top/xiongmingcai/aop/aspect/MethodPro.java
+++ src/main/java/top/xiongmingcai/aop/aspect/MethodPro.java
@@ -3,14 +3,14 @@ package top.xiongmingcai.aop.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 
 public class MethodPro {
-    public void welcome(ProceedingJoinPoint pjp) {
+    public void welcome(ProceedingJoinPoint pjp) throws Throwable {
 
         System.out.println("欢迎光临小店");
         try {
             pjp.proceed();
             System.out.println("欢迎光临下次小店");
         } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            throw throwable;
         }
     }
 }
