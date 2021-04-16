d4bf0f72b1e6901cc0118dfd2265f1e9a87838f7
xmc <xmc000@icloud.com>
2021-04-16 18:28:54 +0800

    @Aspect 切面注解练习4-3

--- src/main/java/top/xiongmingcai/aop/aspect/MethodPro.java
+++ src/main/java/top/xiongmingcai/aop/aspect/MethodPro.java
@@ -1,16 +1,24 @@
 package top.xiongmingcai.aop.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class MethodPro {
-    public void welcome(ProceedingJoinPoint pjp) throws Throwable {
+    @Around("execution( public * top.xiongmingcai..*.*(..))")
+    public Object welcome(ProceedingJoinPoint pjp) throws Throwable {
 
+        Object result;
         System.out.println("欢迎光临小店");
         try {
-            pjp.proceed();
+            result = pjp.proceed();
             System.out.println("欢迎光临下次小店");
         } catch (Throwable throwable) {
             throw throwable;
         }
+        return result;
     }
 }
--- src/main/java/top/xiongmingcai/aop/service/BookShop.java
+++ src/main/java/top/xiongmingcai/aop/service/BookShop.java
@@ -1,5 +1,8 @@
 package top.xiongmingcai.aop.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookShop {
     public void sellingBooks(){
         System.out.println("卖出了一本<<java>>书。");
