2cb3bfd6973b5f1074c7efdd01276fc9e83563dc
xmc <xmc000@icloud.com>
2021-04-16 08:44:54 +0800

    PointCut切点表达式

--- src/main/java/top/xiongmingcai/aop/SpringApplication.java
+++ src/main/java/top/xiongmingcai/aop/SpringApplication.java
@@ -2,6 +2,7 @@ package top.xiongmingcai.aop;
 
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import top.xiongmingcai.aop.service.EmployeeService;
 import top.xiongmingcai.aop.service.UserService;
 
 public class SpringApplication {
@@ -9,6 +10,8 @@ public class SpringApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
         UserService userService = context.getBean("userService", UserService.class);
+        EmployeeService employeeService = context.getBean("employeeService", EmployeeService.class);
+        employeeService.onBoarding();
         userService.createUser();
         userService.validationPassword("MD5" , 16);
     }
