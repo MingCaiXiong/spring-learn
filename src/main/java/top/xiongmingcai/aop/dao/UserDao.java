9abd8b664deb459169808011cb503cfdbfc9716d
xmc <xmc000@icloud.com>
2021-04-16 16:18:04 +0800

    Aop 环绕通知文件准备

--- src/main/java/top/xiongmingcai/aop/dao/UserDao.java
+++ src/main/java/top/xiongmingcai/aop/dao/UserDao.java
@@ -2,9 +2,6 @@ package top.xiongmingcai.aop.dao;
 
 public class UserDao {
     public void insert(){
-       if (true){
-         throw  new RuntimeException("用户已存在") ;
-       }
         System.out.println("新增用户数据");
     }
 }
