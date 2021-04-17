e6d0c37a7e6c3ed9d38ee246bbf1daab1e1cc085
xmc <xmc000@icloud.com>
2021-04-17 12:27:53 +0800

    JdbcTemplate对象中三个查询方法：

--- src/main/java/top/xiongmingcai/jdbc/dao/EmployeeDao.java
+++ src/main/java/top/xiongmingcai/jdbc/dao/EmployeeDao.java
@@ -4,6 +4,9 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import top.xiongmingcai.jdbc.entity.Employee;
 
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * (Employee)表数据库访问层
@@ -21,6 +24,20 @@ public class EmployeeDao {
                 new BeanPropertyRowMapper<Employee>(Employee.class));
         return employee;
     };
+
+    // 查询结果中多条记录转换为对应对象，我们可以使用query()进行查询，
+    public List<Employee> queryByDname(String dname){
+        String sql = "select * from employee where dname = ?;";
+        return jdbcTemplate.query(sql,
+                new Object[]{dname},
+                new BeanPropertyRowMapper<>(Employee.class));
+    }
+    // 很多字段名是没有实体属性的，无法进行实体类映射，我们可以使用queryForList()进行查询，这个结果会被封装成map对象
+    public List<Map<String, Object>>  queryMapByDname(String dname){
+        String sql = "select dname as dnme,  salary as s  from employee where dname = ?";
+        //没有实体类对应的情况下queryForList将查询结果作为map进行封装
+        return jdbcTemplate.queryForList(sql, dname);
+    }
 
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
--- src/test/java/top/xiongmingcai/jdbc/dao/EmployeeDaoTest.java
+++ src/test/java/top/xiongmingcai/jdbc/dao/EmployeeDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import top.xiongmingcai.jdbc.entity.Employee;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
@@ -17,5 +19,19 @@ public class EmployeeDaoTest {
     public void findById() {
         Employee employee = employeeDao.findById(1);
         System.out.println("employee = " + employee);
+    }
+
+    @Test
+    public void queryByDname() {
+        List<Employee> employeeList = employeeDao.queryByDname("市场部");
+        System.out.println(employeeList);
+    }
+
+    @Test
+    public void queryMapByDname() {
+        List<Map<String, Object>> mapList = employeeDao.queryMapByDname("研发部");
+        for (Map<String, Object> stringObjectMap : mapList) {
+            System.out.println(stringObjectMap);
+        }
     }
 }
