ab8b4bfa18773d3529833e288862c5944182ad26
xmc <xmc000@icloud.com>
2021-04-17 10:11:32 +0800

    init

--- src/main/java/SpringApplication.java
+++ src/main/java/SpringApplication.java
@@ -1,11 +1,8 @@
-import entity.Apple;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-        //output
-        //基于静态工厂
-        // 通过无参构建方法构建对象
+
     }
 }
