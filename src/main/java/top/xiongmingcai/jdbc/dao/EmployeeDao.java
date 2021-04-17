95ca11f6a99a27c050e29999afe8e0c8338e93fd
xmc <xmc000@icloud.com>
2021-04-17 13:53:37 +0800

    jdbcTempalate实现增删改

--- src/main/java/top/xiongmingcai/jdbc/dao/EmployeeDao.java
+++ src/main/java/top/xiongmingcai/jdbc/dao/EmployeeDao.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public class EmployeeDao {
     private JdbcTemplate  jdbcTemplate;
 
-    public Employee findById(Integer eno){
+    public Employee findById(Integer eno) {
         String sql = "select * from employee where eno = ?;";
         Employee employee = jdbcTemplate.queryForObject(sql,
                 new Object[]{eno},
@@ -26,14 +26,14 @@ public class EmployeeDao {
     };
 
     // 查询结果中多条记录转换为对应对象，我们可以使用query()进行查询，
-    public List<Employee> queryByDname(String dname){
+    public List<Employee> queryByDname(String dname) {
         String sql = "select * from employee where dname = ?;";
         return jdbcTemplate.query(sql,
                 new Object[]{dname},
                 new BeanPropertyRowMapper<>(Employee.class));
     }
     // 很多字段名是没有实体属性的，无法进行实体类映射，我们可以使用queryForList()进行查询，这个结果会被封装成map对象
-    public List<Map<String, Object>>  queryMapByDname(String dname){
+    public List<Map<String, Object>> queryMapByDname(String dname) {
         String sql = "select dname as dnme,  salary as s  from employee where dname = ?";
         //没有实体类对应的情况下queryForList将查询结果作为map进行封装
         return jdbcTemplate.queryForList(sql, dname);
@@ -46,4 +46,31 @@ public class EmployeeDao {
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+
+    //新增加操作
+    public int insert(Employee employee) {
+        String sql = "INSERT INTO employee ( ename, salary, dname, hiredate) VALUES ( ?, ?, ?, ?)";
+        int updateRows = jdbcTemplate.update(sql,
+                employee.getEname(),
+                employee.getSalary(),
+                employee.getDname(),
+                employee.getHiredate());
+
+        return updateRows;
+    }
+
+    public int update(Employee employee) {
+        String sql = "UPDATE employee  SET      ename = ?,     salary = ?,     dname = ?,     hiredate =? WHERE eno = ?";
+        int update = jdbcTemplate.update(sql, employee.getEname(),
+                employee.getSalary(),
+                employee.getDname(),
+                employee.getHiredate(), employee.getEno());
+        return update;
+
+    }
+    public int delete(Integer eno){
+        String sql = "DELETE FROM imooc.employee WHERE eno = ?";
+        return jdbcTemplate.update(sql, eno);
+    }
+
 }
--- src/test/java/top/xiongmingcai/jdbc/dao/EmployeeDaoTest.java
+++ src/test/java/top/xiongmingcai/jdbc/dao/EmployeeDaoTest.java
@@ -34,4 +34,30 @@ public class EmployeeDaoTest {
             System.out.println(stringObjectMap);
         }
     }
+
+    @Test
+    public void insert() {
+        Employee employee = new Employee();
+        employee.setEname("王晓燕");
+        employee.setSalary(2000);
+        employee.setDname("市场部");
+        employee.setHiredate("2021-02-15");
+        int updateRow = employeeDao.insert(employee);
+        System.out.println("插入多少行 => " + updateRow);
+    }
+
+
+    @Test
+    public void update() {
+        Employee employee = employeeDao.findById(1);
+        employee.setSalary(3000);
+        int updateRow = employeeDao.update(employee);
+        System.out.println("更新多少行 => " + updateRow);
+    }
+
+    @Test
+    public void delete() {
+        int delete = employeeDao.delete(1);
+
+    }
 }
