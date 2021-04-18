5b160b7a9180a276adb74cfe19d2bf7bc8a60711
xmc <xmc000@icloud.com>
2021-04-18 17:31:08 +0800

    @Mapping

---
+++ src/main/java/top/xiongmingcai/springmvc/controller/URLMappingCaontroller.java
@@ -0,0 +1,22 @@
+package top.xiongmingcai.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+// @RequestMapping(value="/g",method = RequestMethod.GET)
+public class URLMappingCaontroller {
+  @GetMapping("/g")
+  @ResponseBody
+  public String getMapping() {
+    return "this is get 方法";
+  }
+
+  @PostMapping("/post")
+  @ResponseBody
+  public String postMapping() {
+    return "this is post 方法";
+  }
+}
