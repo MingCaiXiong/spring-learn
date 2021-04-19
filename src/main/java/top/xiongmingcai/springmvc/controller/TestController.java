e0e0b5ba3885cef1cc210418709e7ce2beea8b83
xmc <xmc000@icloud.com>
2021-04-19 14:12:47 +0800

    整合springframework-3.2.4.RELEASE

--- src/main/java/top/xiongmingcai/springmvc/controller/TestController.java
+++ src/main/java/top/xiongmingcai/springmvc/controller/TestController.java
@@ -2,14 +2,16 @@ package top.xiongmingcai.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
 
-  @GetMapping("/t") // localhost/t
-  @ResponseBody // 直接向响应输出字符串数据，不跳转界面
-  public String test() {
-    return "hello Spring mvc ...";
+  @GetMapping("/test") // localhost/t
+  //  @ResponseBody // 直接向响应输出字符串数据，不跳转界面
+  public ModelAndView test() {
+    ModelAndView mad = new ModelAndView("/test");
+    String hello = "hello Spring Freemarker";
+    return mad.addObject("hello", hello);
   }
 }
