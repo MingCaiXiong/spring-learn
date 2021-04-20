9eca10ae226d3ab6a64db28cb497139fc1ef25be
xmc <xmc000@icloud.com>
2021-04-20 13:41:37 +0800

    拦截器优先级 请求按配置顺序执行 响应逆序

--- src/main/java/top/xiongmingcai/restful/interceptor/MyInterceptor1.java
+++ src/main/java/top/xiongmingcai/restful/interceptor/MyInterceptor1.java
@@ -6,23 +6,21 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MyInterceptor implements HandlerInterceptor {
+public class MyInterceptor1 implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println(request.getRequestURL() + "准备执行---000--");
-//        response.getWriter().println("{}");
-//        return false;
+        System.out.println(request.getRequestURL() + "准备执行---111--");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println(request.getRequestURL() + "目标处理成功---000--");
+        System.out.println(request.getRequestURL() + "目标处理成功---111--");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println(request.getRequestURL() + "相应内容已产生---000--");
+        System.out.println(request.getRequestURL() + "相应内容已产生---111--");
     }
 }
