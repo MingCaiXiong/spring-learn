bacd9b3eb2a16b664fe95dbd0bba0921dfad4537
xmc <xmc000@icloud.com>
2021-04-21 18:07:01 +0800

    在IoC容器初始化时自动扫面四种组件类型注解并完成实例化 @Controller  当前mcv 控制类 @Repository  业务类 dao层  持久层 @Service  服务类 @Component  可以控制所有  不确定的时候使用它就得了

--- src/main/java/top/xiongmingcai/restful/interceptor/LoginInterceptor.java
+++ src/main/java/top/xiongmingcai/restful/interceptor/LoginInterceptor.java
@@ -2,12 +2,13 @@ package top.xiongmingcai.restful.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
     private Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 
