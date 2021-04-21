c2c934701a16a46780d9d92591bb44c64f9302cc
xmc <xmc000@icloud.com>
2021-04-21 08:52:23 +0800

    文件保存拦截器拦截的客户端IP,请求URL,user-agent

---
+++ src/main/java/top/xiongmingcai/restful/interceptor/LoginInterceptor.java
@@ -0,0 +1,37 @@
+package top.xiongmingcai.restful.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    private Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        :用户ip地址,访问的url地址,客户端环境等
+        StringBuffer log = new StringBuffer();
+        log.append(request.getRemoteAddr());
+        log.append("|");
+        log.append(request.getRequestURL());
+        log.append("|");
+        log.append(request.getHeader("user-agent"));
+        this.logger.info(log.toString());
+        System.out.println("LoginInterceptor.preHandle");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
