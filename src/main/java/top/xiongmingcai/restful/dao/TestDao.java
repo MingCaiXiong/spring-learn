db6083ffa94e9790dbc445a8bef636cd5cb1b4e9
xmc <xmc000@icloud.com>
2021-04-22 11:36:51 +0800

    MyBatis-Plus介绍与整合步骤: 2.  实体类继承 BaseMapper<Test>

--- src/main/java/top/xiongmingcai/restful/dao/TestDao.java
+++ src/main/java/top/xiongmingcai/restful/dao/TestDao.java
@@ -1,7 +1,10 @@
 package top.xiongmingcai.restful.dao;
 
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.xiongmingcai.restful.entity.Test;
 
-public interface TestDao {
-     void insert();
+public interface TestDao extends BaseMapper<Test> {
+     //自定义示例方法
+     void  insetSample();
 }
