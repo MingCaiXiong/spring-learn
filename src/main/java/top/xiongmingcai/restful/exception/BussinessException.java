cddbd43b4f674147ebc8432608fee29228a476df
xmc <xmc000@icloud.com>
2021-04-23 15:45:23 +0800

    自定义异常类

---
+++ src/main/java/top/xiongmingcai/restful/exception/BussinessException.java
@@ -0,0 +1,36 @@
+package top.xiongmingcai.restful.exception;
+
+/**
+ * 业务相关异常类
+ */
+public class BussinessException extends RuntimeException {
+    private String code;
+    private String msg;
+
+    /**
+     * Constructs a new runtime exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
+    public BussinessException(String code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
