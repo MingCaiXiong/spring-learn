49ee017eac7c1082318cf2ad71eb76cfd4c63741
xmc <xmc000@icloud.com>
2021-04-23 15:48:59 +0800

    完成 创建会员业务逻辑

---
+++ src/main/java/top/xiongmingcai/restful/entity/Member.java
@@ -0,0 +1,94 @@
+package top.xiongmingcai.restful.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * (Member)实体类
+ *
+ * @author makejava
+ * @since 2021-04-23 08:28:43
+ */
+@TableName("member")
+public class Member implements Serializable {
+    private static final long serialVersionUID = -16888155306578859L;
+    /**
+     * 会员编号
+     */
+    @TableId(type = IdType.AUTO)
+    private Long memberId;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 盐值
+     */
+    private Integer salt;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getSalt() {
+        return salt;
+    }
+
+    public void setSalt(Integer salt) {
+        this.salt = salt;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+}
