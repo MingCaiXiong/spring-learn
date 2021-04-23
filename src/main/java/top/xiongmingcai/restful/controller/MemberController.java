f9d0cfa174127725aa461f97616b8976cdc85eaf
xmc <xmc000@icloud.com>
2021-04-23 11:48:03 +0800

    完成 kaptcha 图片二维码 效验逻辑

--- src/main/java/top/xiongmingcai/restful/controller/MemberController.java
+++ src/main/java/top/xiongmingcai/restful/controller/MemberController.java
@@ -2,12 +2,33 @@ package top.xiongmingcai.restful.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MemberController {
     @GetMapping("/register")//register
     public ModelAndView showReigster() {
         return new ModelAndView("/register");
+    }
+
+    @PostMapping("/registe")
+    @ResponseBody
+    public Map registe(String vc, String username, String password, String nickname, HttpServletRequest request) {
+        String verifyCode = (String) request.getSession().getAttribute("verifyCode");
+        HashMap<String, Object> result = new HashMap<>();
+        if (vc == null || !vc.equalsIgnoreCase(verifyCode)) {
+            result.put("code", "VC01");
+            result.put("msg", "验证码错误");
+        } else {
+            result.put("code", "0");
+            result.put("msg", "success");
+        }
+        return result;
     }
 }
